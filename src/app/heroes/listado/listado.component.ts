35ba989f4eab445703ddfc1f2d3a6452762da346
mjmoreno <200085@iecisa.corp>
2022-05-10 12:05:08 +0200

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitan America'];
+  heroe_borrado: string  = '';
+  
+  borrarHeroe() {
+    this.heroe_borrado = this.heroes.pop() || '';
+  }
+}
